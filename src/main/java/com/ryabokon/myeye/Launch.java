833f14eaeba0675a87450c92245b488cc0921ebd
Archie Swif <artem.ryabokon@gmail.com>
2014-02-09 02:24:19 +0200

    Pie camera command now can be set from launch args

--- src/main/java/com/ryabokon/myeye/Launch.java
+++ src/main/java/com/ryabokon/myeye/Launch.java
@@ -10,11 +10,17 @@ public class Launch {
 		URL camera = new URL("http", "192.168.2.107", 80, "/image.jpg");
 		String path = "d:/images/";
 
+		String command = "raspistill -t 200 -cfx 128:128 -h 1024 -w 1280 -o -";
+		if (args.length != 0 && args[0] != null) {
+			command = args[0];
+		}
+
 		AbstractImageProvider shot = new SingleImageProvider(path, camera);
 		// AbstractImageProvider file = new FileSystemImageProvider(path,
 		// camera);
 		// AbstractImageProvider queue = new QueuedImageProvider(path, camera);
-		//AbstractImageProvider pie = new PieImageProvider(path,"raspistill -t 200 -cfx 128:128 -h 1024 -w 1280 -o -");
+		// AbstractImageProvider pie = new PieImageProvider(path,
+		// command);
 
 		DifferenceFinder eye = new DifferenceFinder(shot);
 		eye.startCapture();
