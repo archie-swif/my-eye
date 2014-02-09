a6044cbb6fab2ff815e12f55efad60f3f95b41bf
archie-swif <artem.ryabokon@gmail.com>
2014-02-09 00:10:29 +0000

    Raspberry Pi camera image getter cmdline command was updated

--- src/main/java/com/ryabokon/myeye/Launch.java
+++ src/main/java/com/ryabokon/myeye/Launch.java
@@ -10,14 +10,13 @@ public class Launch {
 		URL camera = new URL("http", "192.168.2.107", 80, "/image.jpg");
 		String path = "d:/images/";
 
-		AbstractImageProvider shot = new SingleImageProvider(path, camera);
+		//AbstractImageProvider shot = new SingleImageProvider(path, camera);
 		// AbstractImageProvider file = new FileSystemImageProvider(path,
 		// camera);
 		// AbstractImageProvider queue = new QueuedImageProvider(path, camera);
-		// AbstractImageProvider pie = new PieImageProvider(path,
-		// "cmd.exe /C type src\\test\\resources\\1.jpg");
+		AbstractImageProvider pie = new PieImageProvider(path,"raspistill -t 200 -cfx 128:128 -h 1024 -w 1280 -o -");
 
-		DifferenceFinder eye = new DifferenceFinder(shot);
+		DifferenceFinder eye = new DifferenceFinder(pie);
 		eye.startCapture();
 	}
 }
