92a3c9aacfd1a4db6a2cc1ffc5abe67cc27e114e
archie-swif <artem.ryabokon@gmail.com>
2014-02-09 02:12:40 +0200

    Set defauly image provider to single image

--- src/main/java/com/ryabokon/myeye/Launch.java
+++ src/main/java/com/ryabokon/myeye/Launch.java
@@ -10,13 +10,13 @@ public class Launch {
 		URL camera = new URL("http", "192.168.2.107", 80, "/image.jpg");
 		String path = "d:/images/";
 
-		//AbstractImageProvider shot = new SingleImageProvider(path, camera);
+		AbstractImageProvider shot = new SingleImageProvider(path, camera);
 		// AbstractImageProvider file = new FileSystemImageProvider(path,
 		// camera);
 		// AbstractImageProvider queue = new QueuedImageProvider(path, camera);
-		AbstractImageProvider pie = new PieImageProvider(path,"raspistill -t 200 -cfx 128:128 -h 1024 -w 1280 -o -");
+		//AbstractImageProvider pie = new PieImageProvider(path,"raspistill -t 200 -cfx 128:128 -h 1024 -w 1280 -o -");
 
-		DifferenceFinder eye = new DifferenceFinder(pie);
+		DifferenceFinder eye = new DifferenceFinder(shot);
 		eye.startCapture();
 	}
 }
